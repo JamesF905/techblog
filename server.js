d3c82e06a01fa7072798b44cc593c6c428c99250
James <jameslfidlin@gmail.com>
2022-07-25 12:47:54 -0400

    enabled helpers, temporarily disabled custom handlebars extension, toallow for helpers to run smoothly

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 // Set up Handlebars.js engine with custom helpers
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -27,8 +27,11 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('hbs', exphbs({extname:".hbs"}));
-app.set('view engine', 'hbs');
+//app.engine('hbs',  hbs.engine, exphbs({extname:".hbs"}));
+//app.set('view engine', 'hbs');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
