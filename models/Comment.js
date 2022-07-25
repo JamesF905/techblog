a968c82d07ec8816aacdd0445203a46830b63a37
James <jameslfidlin@gmail.com>
2022-07-25 00:32:25 -0400

    added controller index, adjusted comment model

--- models/Comment.js
+++ models/Comment.js
@@ -11,10 +11,6 @@ Comment.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     body: {
       type: DataTypes.TEXT,
     },
@@ -36,7 +32,7 @@ Comment.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'blog',
+    modelName: 'comment',
   }
 );
 
