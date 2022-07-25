d05915459255aeeee1f5895773ca385427c52a16
James <jameslfidlin@gmail.com>
2022-07-25 13:11:21 -0400

    blog handlebars working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+//THIS SECTION RENDERS THE HOMEPAGE
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
@@ -21,6 +22,29 @@ router.get('/', async (req, res) => {
     res.render('home', { 
       blogs, 
       logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//THIS SECTION RENDERS THE BLOGS PAGE
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+      ],
+    });
+
+    const blog = blogData.get({ plain: true });
+
+    res.render('blog', {
+      ...blog,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
