126977b1c89a3d8dbfaf3a2b8202368651f992eb
James <jameslfidlin@gmail.com>
2022-07-27 19:34:15 -0400

    Pushing to Heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./User');
-const Blog = require('./Blog');
-const Comment = require('./Comment');
+const User = require('./user');
+const Blog = require('./ulog');
+const Comment = require('./uomment');
 
 
 User.hasMany(Blog, {
